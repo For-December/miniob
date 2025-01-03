e4e44a2dc57ae2ff21e4dc776cda84065ffa9383
forDecember <1921567337@qq.com>
2025-01-03 05:35:20 +0000

    update: date checker

--- deps/common/time/date.h
+++ deps/common/time/date.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include<string>
+#include <string>
 
-bool check_dateV2(int year, int month, int day);
+bool my_check_date(int year, int month, int day);
 
-int string_to_date(const std::string &str,int32_t & date);
+int string_to_date(const std::string &str, int32_t &date);
 
 std::string date_to_string(int32_t date);
